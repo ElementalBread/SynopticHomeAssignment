2e37e29cd1f8d21a1b6dff86ccc69da264f825fc
Mikel Mangion <73097576+ElementalBread@users.noreply.github.com>
2022-06-22 22:53:21 +0200

    Added API calls to Flights Microservice
    Implemented Api Endpoint for the flights microservice that returns a JSON of serialised objectys of type Flight

--- CommonClasses/Flight.cs
+++ CommonClasses/Flight.cs
@@ -6,27 +6,25 @@ using System.Threading.Tasks;
 
 namespace CommonClasses {
     public class Flight {
-        public  Flight(string flight_Number, string airline, int duration, string airport_name, string departure_gate, string seat_class, string luggage_options, float price) {
+        public  Flight(int flight_Number, string airline, int duration, string origin_airport_name, string destination_airport_name, string seat_class, float price) {
             Guid g = Guid.NewGuid();
             this.flight_Id = g;
             this.flight_Number = flight_Number;
             this.airline = airline;
             this.duration = duration;
-            this.airport_name = airport_name;
-            this.departure_gate = departure_gate;
+            this.origin_airport_name = origin_airport_name;
+            this.destination_airport_name = destination_airport_name;
             this.seat_class = seat_class;
-            this.luggage_options = luggage_options;
             this.price = price;
         }
 
         public Guid flight_Id { get; set; }
-        public string flight_Number { get; set; }
+        public int flight_Number { get; set; }
         public string airline { get; set; }
         public int duration { get; set; }
-        public string airport_name { get; set; }
-        public string departure_gate { get; set; }
+        public string origin_airport_name { get; set; }
+        public string destination_airport_name { get; set; }
         public string seat_class { get; set; }
-        public string luggage_options { get; set; }
         public float price { get; set; }
 
     }

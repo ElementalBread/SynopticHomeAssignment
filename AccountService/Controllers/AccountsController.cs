eb2288d47a0b4004d1a124c12cae7c03570f1a9c
Mikel Mangion <73097576+ElementalBread@users.noreply.github.com>
2022-06-23 21:56:28 +0200

    Refactoring/Added google cloud db
    Added Firestore
    Added login/register with firestore
    Added new booking management
    Refactored booking api into hotel api

---
+++ AccountService/Controllers/AccountsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Google.Cloud.Firestore;
+
+namespace AccountService.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class AccountsController : Controller {
+        private readonly ILogger<AccountsController> _logger;
+        private FirestoreDb db = FirestoreDb.Create("synopticassignmen");
+
+        public AccountsController(ILogger<AccountsController> logger) {
+            _logger = logger;
+        }
+
+        [HttpPost("~/register/{email}/{password}")]
+        public async Task register(string email, string password) {
+            DocumentReference docRef = db.Collection("users").Document(email);
+            Dictionary<string, object> user = new Dictionary<string, object>
+            {
+                { "email", email},
+                { "password", password },
+            };
+            await docRef.SetAsync(user);
+        }
+
+
+        [HttpGet("~/login/{email}/{password}")]
+        public async Task<bool> login(string email, string password) {
+            CollectionReference usersRef = db.Collection("users");
+            QuerySnapshot snapshot = await usersRef.GetSnapshotAsync();
+            foreach (DocumentSnapshot document in snapshot.Documents) {
+                Dictionary<string, object> documentDictionary = document.ToDictionary();
+
+                Console.WriteLine(documentDictionary);
+
+                if (String.Compare(documentDictionary["email"].ToString(),email)==0) {
+                    if (String.Compare(documentDictionary["password"].ToString(), password) == 0) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+    
+}
--- SynopticHomeAssignment/Pages/Index.cshtml.cs
+++ SynopticHomeAssignment/Pages/Index.cshtml.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using CommonClasses;
 
 namespace SynopticHomeAssignment.Pages {
     public class IndexModel : PageModel {
         private readonly ILogger<IndexModel> _logger;
+        private Account _account;
 
         public IndexModel(ILogger<IndexModel> logger) {
             _logger = logger;
         }
 
         public void OnGet() {
+            if(_account == null) {
 
+            }
         }
     }
 }
